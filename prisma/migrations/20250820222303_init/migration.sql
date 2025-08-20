73ecb803210bbe299de840ab76a35ba6cc72877b
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-08-20 19:26:12 -0300

    Clonacion de repositorios,estructura, db, migracion

---
+++ prisma/migrations/20250820222303_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateEnum
+CREATE TYPE "public"."MessageRole" AS ENUM ('user', 'geminiAssistant');
+
+-- CreateTable
+CREATE TABLE "public"."Conversation" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Conversation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Message" (
+    "id" TEXT NOT NULL,
+    "conversationId" TEXT NOT NULL,
+    "role" "public"."MessageRole" NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Conversation_createdAt_idx" ON "public"."Conversation"("createdAt");
+
+-- CreateIndex
+CREATE INDEX "Message_conversationId_createdAt_idx" ON "public"."Message"("conversationId", "createdAt");
+
+-- AddForeignKey
+ALTER TABLE "public"."Message" ADD CONSTRAINT "Message_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "public"."Conversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
